87c356b7ce382b50d2b1327af98e579f69e95606
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2022-06-02 19:36:26 +0200

    403 - 404

---
+++ laraProject/resources/views/errors/404.blade.php
@@ -0,0 +1,32 @@
+@extends('welcome')
+
+@section('title', 'Errore')
+@section('content')
+ 
+<div style="margin-left:25%; margin-right: 25%">
+    <div class="container mt-5 pt-5">
+        <div style= "   text-align: center;
+                        vertical-align: middle;
+                        line-height: 90px;">
+            <h2 style="font-size: 30px;
+                border: 3px solid green;">Errore 404</h2>
+            <p style=""font-size: 70px">La pagina non esiste.</p>
+            
+        </div>
+    </div>
+    <a href="{{route('Home')}}">
+        <img src="{{asset('../storage/app/public/errors/error404.jpg')}}" 
+             style="
+         vertical-align: middle;
+          padding: 15px;
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          border-image: url(border.png) 50 round;
+          width: 50%;
+          height: auto;" 
+          alt=""> 
+    </a>
+         
+</div>  
+@endsection
--- laraProject/resources/views/form_faq.blade.php
+++ laraProject/resources/views/form_faq.blade.php
@@ -19,10 +19,24 @@
         <div>
             {{ Form::label('domanda', 'Domanda') }}
             {{ Form::text('domanda', '', ['class' => 'input', 'id' => 'domanda']) }}
+            @if ($errors->first('domanda'))
+        <ul class="errors">
+            @foreach ($errors->get('domanda') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
         </div>
         <div>
             {{ Form::label('risposta', 'Risposta') }}
             {{ Form::text('risposta', '', ['class' => 'input', 'id' => 'risposta']) }}
+            @if ($errors->first('risposta'))
+        <ul class="errors">
+            @foreach ($errors->get('risposta') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
         </div>
         {{ Form::submit('Conferma', ['class' => 'submit']) }}
         {{ Form::close() }}
